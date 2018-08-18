370d26d3393f629db7901f30b30808ca26ac4250
sykes836 <bernard.sykes836@gmail.com>
2018-08-18 11:24:59 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,21 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+var index = 0
 function init() {
   // Write your JavaScript code inside the init() function
-
+  
+ function onKeyDownHandler(e) {
+    const key = parseInt(e.detail || e.which)
+     if (key === konamiCode[index]) {
+      index++
+       if (index === konamiCode.length) {
+        alert("Hurray!")
+        index = 0
+      }
+    } else {
+      index = 0
+    }
+  }
+  const main = document.querySelector('body');
+  main.addEventListener('keydown', onKeyDownHandler)
 }
