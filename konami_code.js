3bcca30bdba3f29c553ff5e951baaaeda5eaf3bc
sykes836 <bernard.sykes836@gmail.com>
2018-08-18 11:26:22 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -4,4 +4,19 @@ function init() {
   // Write your JavaScript code inside the init() function
    let index = 0;
   let body = document.body;
-  
+  body.addEventListener('keydown', function(e){
+    console.log('yara  ', parseInt(e.which || e.detail));
+    console.log('blab  ', code[index]);
+    if (parseInt(e.which || e.detail) === code[index]) {
+      index++;
+      if (index === code.length) {
+        alert('Congrats!!!');
+        index = 0;
+        return;
+      }
+    } else {
+      index = 0;
+      return;
+    }
+  });
+}
